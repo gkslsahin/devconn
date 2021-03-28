4c7d95e38f0f926ab8618278f9844cc415968b3e
gkslsahin <gkslsahin@gmail.com>
2021-03-28 23:29:57 +0300

    add user model

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 5000
 //connec to Database
 connectDB()
 
+//Init Middleware
+app.use(express.json({ extended: false}))
+
 app.get("/", (req,res)=> res.send("api runnig"))
 
 
