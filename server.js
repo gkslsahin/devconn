e6753b49671a3b7158f879123158a91a7e980d98
Goksel Sahin <gokselsahin@teknotam.com.tr>
2021-03-28 13:02:39 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+
+const connectDB = require("./config/db")
+
+const PORT = process.env.PORT || 5000
+
+
+//connec to Database
+connectDB()
+
+app.get("/", (req,res)=> res.send("api runnig"))
+
+
+//Define routes
+app.use("/api/users", require("./routes/api/users"))
+app.use("/api/auth", require("./routes/api/auth"))
+app.use("/api/posts", require("./routes/api/posts"))
+app.use("/api/profile", require("./routes/api/profile"))
+
+app.listen(PORT, ()=> console.log('Server started on port ${PORT}') )
